0acd3cf9716168ce0ba999bf3b2ab1ec83b7f180
cheikhouyowit <cheikhousy@yowit.sn>
2021-10-29 17:19:14 +0000

    ajout modules account_mov l10n portal_expense portal_leave purchase_approval

---
+++ account_move_reversal/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Account Move Reversal',
+    'author': 'Optesis SA',
+    'version': '13.0.0',
+    'category': 'Tools',
+    'description': """
+                        permet de faire l'annulation en négatif des écritures comptables, conformément aux dispositions du SYSCOHADA.
+                   """,
+    'summary': 'Module d\'annulation en négatif des écritures comptables',
+    'sequence': 9,
+    'depends': ['base', 'account','l10n_pcgo'],
+    'data': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ account_move_reversal/models/account_move.py
@@ -0,0 +1,119 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _reverse_move_vals(self, default_values, cancel=True):
+        ''' Reverse values passed as parameter being the copied values of the original journal entry.
+        For example, debit / credit must be switched. The tax lines must be edited in case of refunds.
+
+        :param default_values:  A copy_date of the original journal entry.
+        :param cancel:          A flag indicating the reverse is made to cancel the original journal entry.
+        :return:                The updated default_values.
+        '''
+        self.ensure_one()
+
+        def compute_tax_repartition_lines_mapping(move_vals):
+            ''' Computes and returns a mapping between the current repartition lines to the new expected one.
+            :param move_vals:   The newly created invoice as a python dictionary to be passed to the 'create' method.
+            :return:            A map invoice_repartition_line => refund_repartition_line.
+            '''
+            # invoice_repartition_line => refund_repartition_line
+            mapping = {}
+
+            # Do nothing if the move is not a credit note.
+            if move_vals['type'] not in ('out_refund', 'in_refund'):
+                return mapping
+
+            for line_command in move_vals.get('line_ids', []):
+                line_vals = line_command[2]  # (0, 0, {...})
+
+                if line_vals.get('tax_line_id'):
+                    # Tax line.
+                    tax_ids = [line_vals['tax_line_id']]
+                elif line_vals.get('tax_ids') and line_vals['tax_ids'][0][2]:
+                    # Base line.
+                    tax_ids = line_vals['tax_ids'][0][2]
+                else:
+                    continue
+
+                for tax in self.env['account.tax'].browse(tax_ids).flatten_taxes_hierarchy():
+                    for inv_rep_line, ref_rep_line in zip(tax.invoice_repartition_line_ids, tax.refund_repartition_line_ids):
+                        mapping[inv_rep_line] = ref_rep_line
+            return mapping
+
+        move_vals = self.with_context(include_business_fields=True).copy_data(default=default_values)[0]
+
+        tax_repartition_lines_mapping = compute_tax_repartition_lines_mapping(move_vals)
+
+        for line_command in move_vals.get('line_ids', []):
+            line_vals = line_command[2]  # (0, 0, {...})
+
+            # ==== Inverse debit / credit / amount_currency ====
+            amount_currency = -line_vals.get('amount_currency', 0.0)
+            balance = line_vals['credit'] - line_vals['debit']
+
+            line_vals.update({
+                'amount_currency': amount_currency,
+                'debit': -line_vals['debit'],
+                'credit': -line_vals['credit'],
+            })
+
+            if move_vals['type'] not in ('out_refund', 'in_refund'):
+                continue
+
+            # ==== Map tax repartition lines ====
+            if line_vals.get('tax_repartition_line_id'):
+                # Tax line.
+                invoice_repartition_line = self.env['account.tax.repartition.line'].browse(line_vals['tax_repartition_line_id'])
+                if invoice_repartition_line not in tax_repartition_lines_mapping:
+                    raise UserError(_("It seems that the taxes have been modified since the creation of the journal entry. You should create the credit note manually instead."))
+                refund_repartition_line = tax_repartition_lines_mapping[invoice_repartition_line]
+
+                # Find the right account.
+                account_id = self.env['account.move.line']._get_default_tax_account(refund_repartition_line).id
+                if not account_id:
+                    if not invoice_repartition_line.account_id:
+                        # Keep the current account as the current one comes from the base line.
+                        account_id = line_vals['account_id']
+                    else:
+                        tax = invoice_repartition_line.invoice_tax_id
+                        base_line = self.line_ids.filtered(lambda line: tax in line.tax_ids.flatten_taxes_hierarchy())[0]
+                        account_id = base_line.account_id.id
+
+                tags = refund_repartition_line.tag_ids
+                if line_vals.get('tax_ids'):
+                    subsequent_taxes = self.env['account.tax'].browse(line_vals['tax_ids'][0][2])
+                    tags += subsequent_taxes.refund_repartition_line_ids.filtered(lambda x: x.repartition_type == 'base').tag_ids
+
+                line_vals.update({
+                    'tax_repartition_line_id': refund_repartition_line.id,
+                    'account_id': account_id,
+                    'tag_ids': [(6, 0, tags.ids)],
+                })
+            elif line_vals.get('tax_ids') and line_vals['tax_ids'][0][2]:
+                # Base line.
+                taxes = self.env['account.tax'].browse(line_vals['tax_ids'][0][2]).flatten_taxes_hierarchy()
+                invoice_repartition_lines = taxes\
+                    .mapped('invoice_repartition_line_ids')\
+                    .filtered(lambda line: line.repartition_type == 'base')
+                refund_repartition_lines = invoice_repartition_lines\
+                    .mapped(lambda line: tax_repartition_lines_mapping[line])
+
+                line_vals['tag_ids'] = [(6, 0, refund_repartition_lines.mapped('tag_ids').ids)]
+        return move_vals
+
+
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    _sql_constraints = [
+        (
+            'check_credit_debit',
+            'CHECK(1=1)',
+            'Wrong credit or debit value in accounting entry !'
+        )
+    ]
